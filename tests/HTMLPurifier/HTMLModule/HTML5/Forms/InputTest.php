149294863700c945cb91605808a767c4f092c5de
xemlock <xemlock@gmail.com>
2020-08-17 22:12:39 +0200

    Add input type=date tests

--- tests/HTMLPurifier/HTMLModule/HTML5/Forms/InputTest.php
+++ tests/HTMLPurifier/HTMLModule/HTML5/Forms/InputTest.php
@@ -411,11 +411,47 @@ class HTMLPurifier_HTMLModule_HTML5_SafeForms_InputTest extends HTMLPurifier_HTM
         );
     }
 
-    public function testDate()
-    {}
+    /**
+     * @param string $input
+     * @param string $expected OPTIONAL
+     * @dataProvider provideDate
+     */
+    public function testDate($input, $expected = null)
+    {
+        $this->testDataProvider($input, $expected);
+    }
 
-    public function testDatetimeLocal()
-    {}
+    public function provideDate()
+    {
+        return array(
+            'input date' => array(
+                '<input type="date" name="foo" value="2005-04-02">',
+            ),
+            'input date empty value' => array(
+                '<input type="date" name="foo" value="">',
+            ),
+            'input date no value' => array(
+                '<input type="date" name="foo" value="">',
+            ),
+
+            'input date datetime value' => array(
+                '<input type="date" name="foo" value="2005-04-02 21:37">',
+                '<input type="date" name="foo" value="2005-04-02">',
+            ),
+            'input date invalid value' => array(
+                '<input type="date" name="foo" value="foo">',
+                '<input type="date" name="foo">',
+            ),
+
+            'input date valid attributes' => array(
+                '<input type="date" disabled inputmode="text" min="2004-04-10" max="2010-04-10" readonly required step="1">',
+            ),
+            'input date invalid attributes' => array(
+                '<input type="date" accept="text/plain" alt="foo" checked dirname="foo.dir" height="10" max="10" maxlength="64" min="0" minlength="0" multiple pattern="[a-z]+" placeholder="foo" size="10" src="foo.png" width="10">',
+                '<input type="date">',
+            ),
+        );
+    }
 
     /**
      * @param string $input
